728d868556d66c05c9d5776cbcb6017a3c07c9ec
Ardian Mulya <ardian.mulya.ft21@mail.umy.ac.id>
2022-10-17 16:51:42 +0700

    Add project files.

---
+++ QuickSort/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSort
+{
+    class Program
+    {
+        private int[] arr = new int[20];
+        private int cmp_count = 0;
+        private int mov_count = 0;
+
+        private int n;
+
+        void read()
+        {
+            while t
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
