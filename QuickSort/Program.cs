77a35ffa3346d4f974247427dbcebf46f2e68e9a
Ardian Mulya <ardian.mulya.ft21@mail.umy.ac.id>
2022-10-17 17:55:57 +0700

    membuat method getsize

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -98,8 +98,13 @@ namespace QuickSort
             Console.WriteLine("\nNumber of comparisons : " +cmp_count);
             Console.WriteLine("\nNumber of data movements : "+cmp_count);
         }
+        int getsize()
+        {
+            return (n);
+        }
         static void Main(string[] args)
         {
+
         }
     }
 }
